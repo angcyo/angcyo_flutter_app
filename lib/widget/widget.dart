876f05f2388794add48717ab5db876a61bc31a7a
angcyo <angcyo@126.com>
2020-05-21 17:19:45 +0800

    + gradient widget

--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 export './flow/my_flow_widget.dart';
 export './web/web_browse_widget.dart';
+export 'gradient_app_bar_widget.dart';
 export 'staggered_grid_widget.dart';
 export 'test_widget.dart';
 export 'well_widget.dart';
