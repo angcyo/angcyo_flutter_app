43d63477bfb9bc82160f14251d0199cd0a916bb5
angcyo <angcyo@126.com>
2020-05-22 15:19:56 +0800

    + toast

--- lib/angcyo_flutter_app.dart
+++ lib/angcyo_flutter_app.dart
@@ -1,6 +1,7 @@
 library angcyo_flutter_app;
 
 export 'package:flutter/material.dart';
+export 'package:oktoast/oktoast.dart';
 
 export 'package:angcyo_flutter_app/widget/widget.dart';
 export 'package:angcyo_flutter_app/util/util.dart';
--- lib/util/util.dart
+++ lib/util/util.dart
@@ -2,3 +2,6 @@ export 'log.dart';
 export 'toast.dart';
 export 'share.dart';
 export 'url.dart';
+
+
+now() => DateTime.now();
