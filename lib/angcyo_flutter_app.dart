b48fd20bdd6c2a6e4a93780669e14c91bdf8bf96
angcyo <angcyo@126.com>
2020-05-26 18:29:22 +0800

    + add library

--- lib/angcyo_flutter_app.dart
+++ lib/angcyo_flutter_app.dart
@@ -1,6 +1,7 @@
 library angcyo_flutter_app;
 
 export 'package:flutter/material.dart';
+export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:oktoast/oktoast.dart';
 
 export 'package:angcyo_flutter_app/widget/widget.dart';
